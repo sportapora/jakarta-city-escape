261e46a62cd04643fed66fc0058025a14ac6bb1c
sportapora <henrysalim22@gmail.com>
2024-05-10 17:55:11 +0700

    fix: Dark layer in explore page; feat: group by category

--- src/Pages/ExploreJakarta.jsx
+++ src/Pages/ExploreJakarta.jsx
@@ -11,8 +11,21 @@ export default function ExploreJakarta() {
     document.getElementById("dark-layer").classList.remove("opacity-0");
     document.getElementById("dark-layer").classList.add("opacity-25");
   };
+
+  const categories = [
+    "Sejarah, Seni & Budaya",
+    "Taman Hiburan dan Edukasi",
+    "Taman Hiburan",
+  ];
+
   const [destinations, setDestinations] = useState([]);
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleFilter = (e, index) => {
+    e.preventDefault();
+    index != null ? setFilter(destinations[index].kategori) : setFilter("");
+  };
 
   useEffect(() => {
     axios
@@ -29,7 +42,7 @@ export default function ExploreJakarta() {
         style={{ backgroundImage: `url(${KotuHero})` }}
         id="hero"
       >
-        <div className="container text-right">
+        <div className="container z-[70] text-right">
           <h1
             className="hero-title transition-transform ease-in-out duration-500 translate-y-72"
             id="hero-title"
@@ -79,34 +92,81 @@ export default function ExploreJakarta() {
         </form>
 
         <div className="lg:container w-full mt-6 grid grid-cols-4 gap-4">
-          {destinations.map((destination, index) => (
+          {categories.map((category, index) => (
             <a
               href=""
+              onClick={(e) => handleFilter(e, index)}
               key={index}
-              class="bg-gray-200 hover:bg-gray-300 text-stone-800 transition-color ease-in-out duration-300 text-center text-xs font-medium me-2 px-4 py-2 rounded-full dark:bg-stone-700 dark:text-gray-300 dark:hover:bg-stone-800"
+              className={`bg-stone-${
+                category === filter ? "500 text-white" : "200"
+              } flex items-center justify-center  hover:bg-stone-600 hover:text-white text-stone-800 transition-color ease-in-out duration-300 text-center text-xs font-medium me-2 px-4 py-2 rounded-full dark:bg-stone-700 dark:text-gray-300 dark:hover:bg-stone-800`}
             >
-              {destination.kategori}
+              {category}
             </a>
           ))}
+          <a
+            href=""
+            onClick={(e) => handleFilter(e, null)}
+            className="bg-red-600 flex items-center justify-center gap-4 text-white hover:bg-red-700 transition-color ease-in-out duration-300 text-center text-xs font-medium me-2 px-4 py-2 rounded-full dark:bg-stone-700 dark:text-gray-300 dark:hover:bg-stone-800"
+          >
+            Hapus filter
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-x"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </span>
+          </a>
         </div>
 
         {destinations.length > 0 ? (
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 mt-16">
-            {destinations.map((destination) => (
-              <a
-                className="hover:-translate-y-4 group ease-in-out duration-300 transition-transform"
-                href={`/jelajahi/${destination.nama
-                  .replace(/\s+/g, "-")
-                  .toLowerCase()}`}
-              >
-                <Card
-                  key={destination.id}
-                  nama={destination.nama}
-                  image={destination.image}
-                  deskripsi={destination.deskripsi.slice(0, 250)}
-                />
-              </a>
-            ))}
+            {filter != ""
+              ? destinations
+                  .filter((destination) => destination.kategori == filter)
+                  .map((destination, index) => (
+                    <a
+                      key={index}
+                      className="hover:-translate-y-4 group ease-in-out duration-300 transition-transform"
+                      href={`/jelajahi/${destination.nama
+                        .replace(/\s+/g, "-")
+                        .toLowerCase()}`}
+                    >
+                      <Card
+                        key={destination.id}
+                        nama={destination.nama}
+                        image={destination.image}
+                        deskripsi={destination.deskripsi.slice(0, 250)}
+                      />
+                    </a>
+                  ))
+              : destinations.map((destination, index) => (
+                  <a
+                    key={index}
+                    className="hover:-translate-y-4 group ease-in-out duration-300 transition-transform"
+                    href={`/jelajahi/${destination.nama
+                      .replace(/\s+/g, "-")
+                      .toLowerCase()}`}
+                  >
+                    <Card
+                      key={destination.id}
+                      nama={destination.nama}
+                      image={destination.image}
+                      deskripsi={destination.deskripsi.slice(0, 250)}
+                    />
+                  </a>
+                ))}
           </div>
         ) : (
           <div className="my-10">
