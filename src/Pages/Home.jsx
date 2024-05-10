0de250509758dc5f6fe9f0834f4fba6d827d099b
sportapora <henrysalim22@gmail.com>
2024-05-10 21:01:25 +0700

    fix: Carousel in Home page; not yet: carousel responsiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,55 +1,27 @@
 import Layout from "../Layout.jsx";
-import MonasHero from "../assets/monas.jpg";
-import KotuHero from "../assets/kotu-hero.jpg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import Carousel from "../Components/Carousel.jsx";
 
 export default function Home() {
-  gsap.registerPlugin(ScrollToPlugin);
-
   window.onload = function () {
     document.getElementById("hero").classList.remove("opacity-0");
+    document.getElementById("dark-layer").classList.remove("opacity-0");
+    document.getElementById("dark-layer").classList.add("opacity-25");
     document
       .getElementById("eksplorasi-btn")
       .classList.remove("translate-y-48");
-
-    let container = document.getElementById("hero"),
-      height = container.offsetHeight,
-      imgs = gsap.utils.toArray(".image"),
-      noImgs = imgs.length,
-      fade = document.querySelector(".fade"),
-      fadedur = 1,
-      fadePause = 0.5,
-      next = 3;
-
-    function crossfade() {
-      gsap
-        .timeline()
-        .to(fade, { autoAlpha: 1, duration: 1 })
-        .set(imgs, { y: "-=" + height })
-        .set(imgs[0], { y: "+=" + height * noImgs })
-        .to(fade, { autoAlpha: 0, duration: fadedur }, "+=" + fadePause);
-
-      imgs.push(imgs.shift());
-
-      gsap.delayedCall(next, crossfade);
-    }
-
-    gsap.delayedCall(next, crossfade);
-  };
-
-  let images = [MonasHero, KotuHero];
-  const [destinations, setDestinations] = useState([]);
-
-  window.onscroll = function () {
     let welcomeElement = document.getElementById("welcome");
 
     if (window.pageYOffset >= 300) {
       welcomeElement.classList.remove("opacity-0");
     }
   };
+  gsap.registerPlugin(ScrollToPlugin);
+  const [destinations, setDestinations] = useState([]);
+  const [images, setImages] = useState([]);
 
   const scrollToWelcome = (event) => {
     event.preventDefault();
@@ -65,82 +37,83 @@ export default function Home() {
       .then((response) => {
         setDestinations(response.data.data);
       });
+
+    axios
+      .get("http://localhost:3000/jakartacityescape/api/getCarouselImages")
+      .then((response) => {
+        setImages(response.data.data);
+      });
   }, []);
 
   return (
     <Layout>
-      <div className="relative h-screen">
-        <div className="h-screen w-full overflow-hidden relative" id="hero">
-          {images.map((image, index) => (
-            <img
-              src={`${image}`}
-              key={index}
-              className="relative transition-all h-screen w-full ease-in-out object-cover object-center image"
-            />
-          ))}
-          <div className="fade absolute hidden top-0 left-0 w-full h-full bg-transparent"></div>
-          <div className="absolute left-1/2 right-1/2 flex justify-center top-1/2">
-            <form className="container w-full lg:w-1/2 mx-auto">
-              <label
-                htmlFor="default-search"
-                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-              >
-                Search
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Mau pergi ke mana hari ini?"
-                />
-                <button
-                  type="submit"
-                  className="text-white hidden md:block absolute end-2.5 bottom-2.5 btn-primary font-medium rounded-lg text-sm px-4 py-2 "
+      <div
+        className="relative h-screen transition-opacity ease-in-out duration-500"
+        id="hero"
+      >
+        <Carousel images={images} />
+        <div className="absolute z-[70] flex w-full flex-col items-center justify-center top-1/2">
+          <form className="container w-full lg:w-1/2 mx-auto">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
                 >
-                  Search
-                </button>
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
               </div>
-
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Mau pergi ke mana hari ini?"
+              />
               <button
                 type="submit"
-                className="text-white block md:hidden mt-2 w-full end-2.5 bottom-2.5 btn-primary font-medium rounded-lg text-sm px-4 py-2 "
+                className="text-white hidden md:block absolute end-2.5 bottom-2.5 btn-primary font-medium rounded-lg text-sm px-4 py-2 "
               >
                 Search
               </button>
-            </form>
-          </div>
-          <div className="absolute bottom-2 right-1/2 flex justify-center">
-            <a
-              href=""
-              id="eksplorasi-btn"
-              onClick={scrollToWelcome}
-              className="bg-transparent transition-transform ease-in-out duration-500 translate-y-48 text-center text-white font-bold text-2xl bottom-0 p-4 rounded-lg absolute"
+            </div>
+
+            <button
+              type="submit"
+              className="text-white block md:hidden mt-2 w-full end-2.5 bottom-2.5 btn-primary font-medium rounded-lg text-sm px-4 py-2 "
             >
-              Yuk eksplorasi <span className="font-bold">Jakarta</span>!
-              <span className="text-3xl block motion-safe:animate-bounce">
-                &darr;
-              </span>
-            </a>
-          </div>
+              Search
+            </button>
+          </form>
         </div>
+        <div className="absolute flex justify-center w-1/2 left-1/4 z-[70] bottom-5">
+          <a
+            href=""
+            id="eksplorasi-btn"
+            onClick={scrollToWelcome}
+            className="bg-transparent transition-transform ease-in-out duration-500 translate-y-48 text-center text-white font-bold text-2xl bottom-0 p-4 rounded-lg absolute"
+          >
+            Yuk eksplorasi <span className="font-bold">Jakarta</span>!
+            <span className="text-3xl block motion-safe:animate-bounce">
+              &darr;
+            </span>
+          </a>
+        </div>
+        <div className="dark-layer opacity-0" id="dark-layer"></div>
       </div>
 
       <div
