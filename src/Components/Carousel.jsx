c3b0a90168bd8b747d75df74cdf842f8dfe37712
sportapora <henrysalim22@gmail.com>
2024-05-13 16:00:44 +0700

    fix carousel bug

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,8 +3,8 @@ import KotuHero from "../assets/kotu-hero.jpg";
 import TMIIHero from "../assets/tmii.png";
 import BundHI from "../assets/BundHI.jpg";
 import musMac from "../assets/museumMacan.jpg";
-import bundar from "../assets/tesBundh.webp"
-import Hutan from "../assets/HutanKotaSCBD.jpeg"
+import bundar from "../assets/tesBundh.webp";
+import Hutan from "../assets/HutanKotaSCBD.jpeg";
 
 export default function Carousel() {
   return (
@@ -37,30 +37,30 @@ export default function Carousel() {
             w-full h-full object-cover object-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             alt="Destinasi Wisata"
           />
-          <div class="duration-700 ease-in-out" data-carousel-item>
-            <img
-              src={BundHI}
-              class="absolute
+        </div>
+        <div class="duration-700 ease-in-out" data-carousel-item>
+          <img
+            src={BundHI}
+            class="absolute
             w-full h-full object-cover object-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="Destinasi Wisata"
-            />
-          </div>
-          <div class="duration-700 ease-in-out" data-carousel-item>
-            <img
-              src={musMac}
-              class="absolute
+            alt="Destinasi Wisata"
+          />
+        </div>
+        <div class="duration-700 ease-in-out" data-carousel-item>
+          <img
+            src={musMac}
+            class="absolute
             w-full h-full object-cover object-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="Destinasi Wisata"
-            />
-          </div>
-          <div class="duration-700 ease-in-out" data-carousel-item>
-            <img
-              src={bundar}
-              class="absolute
+            alt="Destinasi Wisata"
+          />
+        </div>
+        <div class="duration-700 ease-in-out" data-carousel-item>
+          <img
+            src={bundar}
+            class="absolute
             w-full h-full object-cover object-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="Destinasi Wisata"
-            />
-          </div>
+            alt="Destinasi Wisata"
+          />
         </div>
         <div class="duration-700 ease-in-out" data-carousel-item>
           <img
