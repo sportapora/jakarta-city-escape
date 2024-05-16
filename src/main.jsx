169938e3a0511424c86f29411552d3c7486d69a7
sportapora <henrysalim22@gmail.com>
2024-05-16 14:19:25 +0700

    updated routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./Pages/Home.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage.jsx";
 import ExploreJakarta from "./Pages/ExploreJakarta.jsx";
 import DestinationDetail from "./Pages/DestinationDetail.jsx";
 import AboutDevs from "./Pages/AboutDevs.jsx";
-import ErrorPage from "./Pages/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/jelajahi",
+    element: <ExploreJakarta />,
+  },
+  {
+    path: "/jelajahi/:namaDestinasi",
+    element: <DestinationDetail />,
+  },
+  {
+    path: "/about-devs",
+    element: <AboutDevs />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
-        <Route path="/jelajahi" element={<ExploreJakarta />} />
-        <Route
-          path="/jelajahi/:namaDestinasi"
-          element={<DestinationDetail />}
-        />
-        <Route path="/about-devs" element={<AboutDevs />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
